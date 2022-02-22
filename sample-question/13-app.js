682f8885b377b02259d091d7f27cbee21502f3fd
Elif Yildiz <elifyca@gmail.com>
2022-02-22 10:27:28 -0800

    added

---
+++ sample-question/13-app.js
@@ -0,0 +1,64 @@
+function productNumbers(array){
+    let array2 = []
+    for (let i = 0; i < array.length; i++) {
+        let number = 1
+        for (let j = 0; j < array.length; j++) {
+            if(i !== j){
+                number *= array[j]
+            }
+        }
+        array2.push(number)
+    }
+    return array2
+}
+
+array = [1, 2, 3, 4, 5]
+console.log(productNumbers(array));
+function productNumbers(array){
+    let array2 = []
+    for (let i = 0; i < array.length; i++) {
+        let number = 1
+        for (let j = 0; j < array.length; j++) {
+            if(i !== j){
+                number *= array[j]
+            }
+        }
+        array2.push(number)
+    }
+    return array2
+}
+
+array = [1, 2, 3, 4, 5]
+console.log(productNumbers(array));
+function productNumbers(array){
+    let array2 = []
+    for (let i = 0; i < array.length; i++) {
+        let number = 1
+        for (let j = 0; j < array.length; j++) {
+            if(i !== j){
+                number *= array[j]
+            }
+        }
+        array2.push(number)
+    }
+    return array2
+}
+
+array = [1, 2, 3, 4, 5]
+console.log(productNumbers(array));
+function productNumbers(array){
+    let array2 = []
+    for (let i = 0; i < array.length; i++) {
+        let number = 1
+        for (let j = 0; j < array.length; j++) {
+            if(i !== j){
+                number *= array[j]
+            }
+        }
+        array2.push(number)
+    }
+    return array2
+}
+
+array = [1, 2, 3, 4, 5]
+console.log(productNumbers(array));
